ee467ee9aeb328239b21e4666fb12f690623961d
ranss@rambler.ru <ranss@rambler.ru>
2023-07-30 13:35:24 +0300

    init commit

---
+++ src/main/java/com/example/coursework2c/CourseWork2cApplication.java
@@ -0,0 +1,13 @@
+package com.example.coursework2c;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWork2cApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseWork2cApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/coursework2c/CourseWork2cApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coursework2c;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWork2cApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
